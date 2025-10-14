c145ab53ff74594160dd8f169639aa11e348d950
JactusTheCactus <threateninglyamusing@gmail.com>
2025-10-14 05:02:47 -0700

    Update .sh

--- .sh
+++ .sh
@@ -6,7 +6,7 @@ flag() {
 	done
 }
 ymlToJson() {
-	yq 'del(.["_"]?, .__*, ._template?, ._defaults?)' -o=json "$1.yml" > "$1.json"
+	yq eval 'del(.[] | select(tag == "!!map") | with_entries(select(.key | test("^_"))))' "$1.yml" -o=json > "$1.json"
 }
 SCRIPT=app
 BIN=bin
