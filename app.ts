55529b1c51f3b9491c6326db13b8f4bd9737fe6e
JactusTheCactus <threateninglyamusing@gmail.com>
2025-10-15 05:57:59 -0700

    Update app.ts

--- app.ts
+++ app.ts
@@ -22,7 +22,7 @@ const format = Intl.NumberFormat('en-CA', {
 function fmt(n: number): string {
 	return format.format(n)
 }
-const title = "{{game_name}}"
+const title = "{{game.title}}"
 class State {
 	run: boolean
 	keys: Array<string | number>
